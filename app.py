ca6ad5da38f6583250a1618289a36cf14b877898
rupin <patelrupin63@gmail.com>
2021-07-14 01:01:38 +0530

    update app.py

--- app.py
+++ app.py
@@ -28,13 +28,7 @@ ELEMENTS_PER_PAGE = 50
 config = {
 
 
-    "apiKey": "AIzaSyDtqr58_Sfj_KBkUxlVtCiDHQATf73yzNw",
-    "authDomain": "test-c1ccd.firebaseapp.com",
-    "databaseURL": "https://test-c1ccd-default-rtdb.firebaseio.com",
-    "projectId": "test-c1ccd",
-    "storageBucket": "test-c1ccd.appspot.com",
-    "messagingSenderId": "638404195953",
-    "appId": "1:638404195953:web:ff87b33a0637940e9b0dd0"
+ <Firebase Configurations>
 }
 
 #initialize firebase
